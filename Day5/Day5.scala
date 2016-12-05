2a4d742a49345bece98733057024ac67464519ca
Marko Stamenovic <markostam@gmail.com>
2016-12-05 03:58:16 -0500

    about halfway thorugh part 1

--- Day5/Day5.scala
+++ Day5/Day5.scala
@@ -4,6 +4,31 @@ def md5(s: String) = {
   MessageDigest.getInstance("MD5").digest(s.getBytes)
 }
 
-val DoorID = "uqwqemis"
+val doorID = "uqwqemis"
 
-val thing = md5(hash).map(_.toHexString)
+def hash(s: String) : Array[String] = {
+  md5(s.toString).map(x => if (x*(-1) > 0) (x+256).
+  toHexString else x.toHexString)
+}
+
+val thing = 1 to 100000 toArray
+
+lazy val thing2 = (1 to 10000).toArray.map(_ + doorID).map(x => hash(x)).
+  filter(x => (x(0)=="0" & x(1)=="0" & Integer.parseInt(x(2), 16) < 10)).map(_(2))
+
+val thing3 = thing2.map(x => x(1)+x(2)+x(3)+x(4)+x(5)+x(6)+x(7)+x(8))
+
+var i = 1
+var output = ""
+
+def decode_hash (doorID: String, i:Int, output:String) {
+  val test = hash(i + doorID)
+  if (output.length < 8) {
+    if (test(0) == "0" & test(1) == "0" & test(2).toInt < 10) {
+      output.concat(test(2))
+      i = i + 1
+      decode_hash(doorID, i, output)
+    }
+  }
+  output
+}

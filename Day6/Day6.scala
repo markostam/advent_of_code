e84cee450ad7451e7bdd8b331fa5535cf855dd19
Marko Stamenovic <markostam@gmail.com>
2016-12-06 01:25:19 -0500

    day6 done

--- Day6/Day6.scala
+++ Day6/Day6.scala
@@ -5,7 +5,6 @@ object Advent6 {
       map(_.groupBy(_.toChar)).map(x => x.map(y => (y._1, y._2.length))).
       map(x => (x.maxBy(_._2),x.minBy(_._2))).
       map(x => x._1._1.toString+x._2._1.toString).
-      transpose.map(_.mkString("")).foreach(println("Answer: "))
-
+      transpose.map(_.mkString("")).foreach(println)
     }
 }
